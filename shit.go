cda79aa5fc43de68aad84ca069ddaa48c8f053fa
Emanuel Donalds <emanueldonalds@gmail.com>
2025-05-22 21:15:19 +0300

    Parse command

--- shit.go
+++ shit.go
@@ -3,23 +3,46 @@ package main
 import "fmt"
 import "os"
 
+type Action int64
+
+const (
+    ADD Action = iota
+    FLUSH
+)
+
+type Command struct {
+    action Action
+    args []string
+}
+
 func main() {
     var _ = parse_args()
 }
 
 func parse_args() string {
     if len(os.Args) < 2 {
-        fmt.Println("Usage:\n\nshit add <filename>\tAdd a file to the the index\nshit flush <filename>\tWrite the current index to a commit")
-        os.Exit(1)
+        exit()
     }
+
+
+    var action Action
 
     //Arg 1 is the action
-    var action = os.Args[1]
-    fmt.Printf("Action is %s\n", action)
-
-    for i, arg := range(os.Args[2:]) {
-        fmt.Printf("Arg %d is %s\n", i, arg)
+    switch os.Args[1] {
+    case "add":
+        action = ADD
+    case "flush":
+        action = FLUSH
+    default:
+        exit()
     }
 
+    fmt.Printf("Action is %d\n", action)
+
     return "hi"
+}
+
+func exit() {
+        fmt.Println("Usage:\n\nshit add <filename>\tAdd a file to the the index\nshit flush <filename>\tWrite the current index to a commit")
+        os.Exit(1)
 }
